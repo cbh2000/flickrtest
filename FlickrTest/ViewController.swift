f570e529e80a312dffec6a7978a4326ad27b4e6d
Bryan Henderson <bhenderson@izeni.com>
2016-09-20 08:04:18 -0600

    Initial commit. Empty project.

---
+++ FlickrTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrTest
+//
+//  Created by Christopher Bryan Henderson on 9/20/16.
+//  Copyright © 2016 Christopher Bryan Henderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
